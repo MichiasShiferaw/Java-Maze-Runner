475884973837cc6f67e7d58593a7d6e6216b94dd
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-01-15 22:48:10 -0500

    Update Mazerecursion.java

--- Mazerecursion.java
+++ Mazerecursion.java
@@ -10,3 +10,15 @@ public class Mazerecursion {
    public static int[] findStrt(char[][] maze) {
     
     int[] startPoint = new int[2];
+    
+    for (int x = 0; x < maze.length; x++) {
+      for (int y = 0; y < maze[0].length; y++) {
+        
+        if (maze[x][y] == ('S')) {
+          startPoint[0] = x;
+          startPoint[1] = y;
+                  
+          return startPoint;
+        } else {
+          startPoint[0] = -1;
+        }
