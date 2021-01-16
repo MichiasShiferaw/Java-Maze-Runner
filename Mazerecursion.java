936d493cdc0a75713ce33be4e33790eb3e5b4e11
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-01-15 22:47:23 -0500

    Create Mazerecursion.java

---
+++ Mazerecursion.java
@@ -0,0 +1,7 @@
+//Scan all primitive data types
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
